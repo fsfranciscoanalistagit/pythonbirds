cad07d7a3eb03687ab7479938eab28fa3064c4d6
Francisco Sales <franciscosales2007@hotmail.com>
2021-04-12 23:13:03 -0300

    Criados os atributos de instância nome, idade e altura

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,20 @@
 class Pessoa():
+    def __init__(self, nome = None, idade = 0):
+        self.nome = nome
+        self.idade = idade
+        self.altura = 1.85
+
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    p = Pessoa('Sales', 35)
+    # print(Pessoa.cumprimentar(p))
+    # print(id(p))
+    # print(p.cumprimentar())
+    p.nome = 'Francisco'
+    p.altura = 1.85
+    print(p.nome, p.idade, p.altura)
+
+
 
