4aa60e08b297067e2bf2d2b1a185d71214fecda2
Francisco Sales <franciscosales2007@hotmail.com>
2021-04-19 16:36:27 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,20 +1,24 @@
 class Pessoa():
-    def __init__(self, nome = None, idade = 0):
+    def __init__(self, *filhos, nome = None, idade = 0):
         self.nome = nome
         self.idade = idade
         self.altura = 1.85
+        self.filhos = list(filhos)
 
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 if __name__ == '__main__':
-    p = Pessoa('Sales', 35)
+    Filhos = Pessoa(nome = 'Daniel', idade= 5)
     # print(Pessoa.cumprimentar(p))
     # print(id(p))
     # print(p.cumprimentar())
-    p.nome = 'Francisco'
-    p.altura = 1.85
-    print(p.nome, p.idade, p.altura)
+    Fco = Pessoa(Filhos)
+    Fco.nome = 'Francisco'
+    Fco.altura = 1.85
+    print(Fco.nome, Fco.altura)
+    for filho in Fco.filhos:
+        print(filho.nome, filho.idade)
 
 
 
